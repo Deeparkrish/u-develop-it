2318fa497571ee54446c54e4cbdc301c30a2bc3a
Deepa <kosalai17@gmail.com>
2021-07-15 17:08:35 -0700

    get data from server

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,13 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
- CREATE TABLE candidates( id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE parties(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE candidates( id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
     party_id INTEGER,
@@ -9,8 +15,4 @@ DROP TABLE IF EXISTS parties;
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
     );
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
+
